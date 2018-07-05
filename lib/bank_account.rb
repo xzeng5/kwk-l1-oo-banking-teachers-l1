b21107f13a46d178fb16d59aa68cab855968fe32
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-05 17:22:15 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
 class BankAccount
   # code here
+  attr_reader :name, :balance, :status, :deposit
+
+  def initialize (name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
+
+  def balance= (balance)
+    @balance = balance
+  end
+
+  def status= (status)
+    @status = status
+  end
+
+  def deposit(num)
+    @balance = @balance + num
+  end
+
+  def display_balance
+    "Your Balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "closed" || @balance == 0
+      false
+    else
+      true
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
 class Transfer
   # code here
+  attr_reader :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+
+  def valid?
+    if @sender == nil || @receiver == nil
+      false
+    else
+      true
+    end
+  end
+
+  def transfer_class
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    @sender.balance = 950
+    @receiver.balance = 1050
+    @status = "complete"
+
+    if @amount = 4000
+      "Transaction rejected. Please check your account balance."
+      @status = "rejected"
+    end
+
+  end
+
 end
